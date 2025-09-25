0ed57b36b5347ff0c240efbeca41ec618e80a3b9
Hontans <hontanssylvain@gmail.com>
2025-09-25 08:47:36 +0200

    feat: Implement db_connect function for database connection

---
+++ template/ini.php
@@ -0,0 +1,17 @@
+<?php
+    if (!function_exists('db_connect')) {
+        function db_connect()
+        {
+            $dsn = 'mysql:host=localhost;dbname=m2l';
+            $user = 'root';
+            $password = '';
+            try{
+                $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $ex){
+                die("Erreur lors de la connexion SQL : " . $ex->getMessage());
+            }
+            return $dbh;
+        }
+    }
+?>
