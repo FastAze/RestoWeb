53d1707d38a1b9c0665f118c6f9e73ecbf9e1f1a
Hontans <hontanssylvain@gmail.com>
2025-09-25 08:51:50 +0200

    feat: Add footer and path definition files for website structure

---
+++ template/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <p>&copy; 2025 RESTOWEB - HONTANS Sylvain et CHAILLOU Baptiste.</p>
+</footer>
---
+++ template/path.php
@@ -0,0 +1,3 @@
+<?php
+    define('path', dirname($_SERVER['SCRIPT_NAME']));
+?>
