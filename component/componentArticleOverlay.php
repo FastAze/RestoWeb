b118486dee06e9f283eece3b7ada94d3852070cf
Hontans <hontanssylvain@gmail.com>
2025-10-10 13:44:30 +0200

    fix: Add minimum value to quantity input and update SQL dump with new data and triggers

--- component/componentArticleOverlay.php
+++ component/componentArticleOverlay.php
@@ -5,7 +5,7 @@
     <h2 class="voir-article-nom"></h2>
     <div class="voir-article-prix"></div>
     <div class="voir-article-btns">
-        <input type="number" placeholder="1">
+        <input type="number" placeholder="1" min="1">
         <button class="valider-btn" onclick="window.location.href='connection.php'"><p>Valider</p></button>
         <button id="closeVoirArticle" class="retour-btn"><p>Retour</p></button>
     </div>
